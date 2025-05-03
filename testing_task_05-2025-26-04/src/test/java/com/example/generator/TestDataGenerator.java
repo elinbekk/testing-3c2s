612592ccef20430e3eac188356883d76229872fe
Elina <elinanurgal@gmail.com>
2025-05-03 11:19:14 +0300

    master. added task 5 with data generator

--- testing_task_05-2025-26-04/src/test/java/com/example/generator/TestDataGenerator.java
+++ testing_task_05-2025-26-04/src/test/java/com/example/generator/TestDataGenerator.java
@@ -1,7 +1,6 @@
 package com.example.generator;
 
 import com.example.model.PostData;
-import com.example.model.UserData;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import net.datafaker.Faker;
@@ -16,22 +15,11 @@ public class TestDataGenerator {
     private static final ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
 
     public static void main(String[] args) throws IOException {
-        generateUsers(5, "users.json");
-        generatePosts(5, "posts.json");
+        int postCount = 5;
+        generatePosts(postCount);
     }
 
-    private static void generateUsers(int count, String filename) throws IOException {
-        List<UserData> users = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            users.add(new UserData(
-                    String.format("%s%s", faker.name().username().replace(".", ""), faker.number().digits(3)), // Уникальный username
-                    generateSecurePassword() // Сложный пароль
-            ));
-        }
-        mapper.writeValue(new File(filename), users);
-    }
-
-    private static void generatePosts(int count, String filename) throws IOException {
+    private static void generatePosts(int count) throws IOException {
         List<PostData> posts = new ArrayList<>();
         for (int i = 0; i < count; i++) {
             posts.add(new PostData(
@@ -39,10 +27,6 @@ public class TestDataGenerator {
                     faker.lorem().paragraph(3)
             ));
         }
-        mapper.writeValue(new File(filename), posts);
-    }
-
-    private static String generateSecurePassword() {
-        return faker.internet().password(8, 16, true, true, true);
+        mapper.writeValue(new File("posts.json"), posts);
     }
 }
